b51263e72aea013a63492975e76072a1f0f71ec5
Ian Ku <iankuyisien@gmail.com>
2024-03-14 16:24:32 -0700

    refactored code in fast api instead of flask, added session tracking feature to combat race condition

--- main.py
+++ main.py
@@ -1,26 +1,33 @@
-import flask
-from flask import Blueprint, Flask, jsonify, send_file, request
+from typing import List
 import pyodbc
-from flask_cors import CORS
 from collections import defaultdict
 import win32com.client
 import win32com
-
-from typing import List
-from pydantic import BaseModel
+from datetime import datetime
+import shutil
 from pathlib import Path
-import waitress
 
+from pydantic import BaseModel
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
+
+app = FastAPI(openapi_url=None)
+patient_dict = defaultdict()
+queried = False
 PATH_TO_THIS_FOLDER = Path(__file__).resolve().parent
+PATH_TO_SESSIONS_FOLDER = PATH_TO_THIS_FOLDER / "sessions"
 PPT_FILE = Path(PATH_TO_THIS_FOLDER, "stdbatt_v2022.pptm").resolve()
 BAS_FILE = Path(PATH_TO_THIS_FOLDER, "modMain.bas").resolve()
-OUT_FILE = Path(PATH_TO_THIS_FOLDER, "output.pptm").resolve()
-HOST = "127.0.0.1"
-PORT = "4997"
-URL_PREFIX = "/zigzag_backend"
 
-bp = Blueprint(
-    "main_blueprint", __name__, static_folder="static", template_folder="templates"
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 SQLMasterData = (
@@ -31,6 +38,7 @@ SQLMasterData = (
     "DataTypeCompatibility=80;"
     "MARS Connection=True;"
 )
+
 cnxn = pyodbc.connect(
     "DRIVER={SQL Server};"
     "Provider=SQLOLEDB;"
@@ -41,19 +49,37 @@ cnxn = pyodbc.connect(
     "MARS Connection=True;"
 )
 
-patient_dict = defaultdict()
-queried = False
 
-class Patient(BaseModel):
-    id: int
+def timestamp_now(compact=False, only_ymd=False) -> str:
+    """Credit: Brandon
+        Returns a string of the current date+time in the form of
+        YYYY-MM-DD hh:mm:ss
+    If `compact` == True, then returns in the form of
+        YYYYMMDD_hhmmss
+    If `only_ymd` == True, then only the first "year/month/day" portion is returned:
+        YYYY-MM-DD or YYYYMMDD
+    """
+    timestamp = datetime.now()
+    if compact:
+        if only_ymd:
+            return timestamp.strftime("%Y%m%d")
+        return timestamp.strftime("%Y%m%d_%H%M%S")
+    if only_ymd:
+        return timestamp.strftime("%Y-%m-%d")
+    return timestamp.strftime("%Y-%m-%d %H:%M:%S")
+
+class Patients(BaseModel):
+    patients: List[int]
+
+class Visits(BaseModel):
     visits: List[int]
 
+class RequestedZigzag(BaseModel):
+    p_id: int
+    visits: List[int]
 
-class PatientData(BaseModel):
-    datas: List[Patient]
-
-@bp.route("/get_patients", methods=["GET"])
-def get_patient():
+@app.get("/get_patients")
+async def get_patient():
     global queried
     global patient_dict
     cursor = cnxn.cursor()
@@ -71,39 +97,39 @@ def get_patient():
         queried = True
         print('Queried Complete!')
 
-    response = flask.jsonify(patients=list(patient_dict.keys()))
-    response.headers.add("Access-Control-Allow-Origin", "*")
-    return response
+    return Patients(patients=list(patient_dict.keys()))
 
 
-@bp.route("/get_visits", methods=["GET"])
-def get_visits():
-    args = request.args
-    patient_id = args.get("patient_id")
-    response = jsonify(visits=patient_dict[int(patient_id)])
-    response.headers.add("Access-Control-Allow-Origin", "*")
-    print("Visits request completed ")
-    return response
+@app.get("/get_visits")
+async def get_visits(p_id: int):
+    return Visits(visits=patient_dict[int(p_id)])
 
 
-@bp.route("/get_zigzag", methods=["POST"])
-def get_zigzag():
+@app.post("/get_zigzag")
+async def get_zigzag(request: RequestedZigzag):
     try:
-        data = request.json  # Assuming the data is in JSON format
-        print(f'Requested data: {data}')
-
         global path_to_pptx
 
-        visits = data.get("visits")
-        p_id = data.get("patient_id")
+        p_id = request.p_id
+        visits = list(request.visits)
+        print(f'Requested data: {p_id}, {request.visits}')
+
+        cur_time = timestamp_now(compact=True)
+        session_id = f"{cur_time}-{'-'.join(map(str, list(request.visits)))}"
+        this_session_folder = PATH_TO_SESSIONS_FOLDER / session_id
+        
+        # make new folder for session
+        this_session_folder.mkdir()
+
+        # copy operation
+        destination = this_session_folder / f"{session_id}.pptm"
+        destination.write_bytes(PPT_FILE.read_bytes())
 
         ppt = win32com.client.Dispatch("PowerPoint.Application")
 
-        # Process the data as needed
-        response_data = {"message": "Data received successfully"}
-        wb = ppt.Presentations.Open(PPT_FILE)
-        # Original location: \\marcfs\Database\Reports\ZigZag\AutoZigZagChartSQL C2.ppt
-
+        wb = ppt.Presentations.Open(destination)
+        # Original location: \\marcfs\Database\Reports\ZigZag\stdbatt_v2022.pptm
+        
         ppt.VBE.ActiveVBProject.VBComponents.Import(BAS_FILE)
         # Original location: \\marcfs\Database\Reports\ZigZag\StdBatt_v2022_Vue.JS\modMain.bas
 
@@ -121,24 +147,20 @@ def get_zigzag():
         print("Completed Zig Zag")
 
         # Save the PowerPoint file to a temporary location
-        wb.SaveAs(OUT_FILE)
+        wb.SaveAs(destination)
         ppt.Quit()
 
         print("Sending Powerpoint File.")
+
+        headers = {
+            "Access-Control-Expose-Headers": "*"
+        }
         
-        return send_file(OUT_FILE, as_attachment=True, download_name='output.pptm')
+        return FileResponse(path=destination, headers=headers, filename=f'{session_id}.pptm')
 
     except Exception as e:
         print("Error Loading Zig Zag", str(e))
-        return jsonify({"error": str(e)}), 500
+        return ({"detail": "Not Found", 
+                 "error": str(e)})
 
-flask_app = Flask(__name__)
-CORS(flask_app)  # Enable CORS for all routes
-flask_app.config["APPLICATION_ROOT"] = URL_PREFIX
-flask_app.register_blueprint(bp, url_prefix=URL_PREFIX)
 
-if __name__ == "__main__":
-    print(f"Attempting to serve on http://{HOST}:{PORT}{URL_PREFIX}")
-
-    listenStr = f"{HOST}:{PORT}"
-    waitress.serve(flask_app, listen=listenStr, log_untrusted_proxy_headers=True)
